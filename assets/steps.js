c4b3529dc5e6a03d57338235db9610c83fb5a67b
DevarajB97 <94118453+DevarajB97@users.noreply.github.com>
2023-09-06 08:47:49 +0000

    stepsadded

--- assets/steps.js
+++ assets/steps.js
@@ -1,7 +1,3 @@
-function preventBack() { window.history.forward(); }
-setTimeout("preventBack()", 0);
-window.onunload = function () { null };
-
 (function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const d of i.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&a(d)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();const r=document.querySelector("#chat-input"),g=document.querySelector("#send-btn"),s=document.querySelector(".chat-container"),l=document.querySelector("#theme-btn"),y=document.querySelector("#delete-btn");let c=null;const f="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",h=()=>{const t=localStorage.getItem("themeColor");document.body.classList.toggle("light-mode",t==="light_mode"),l.innerText=document.body.classList.contains("light-mode")?"dark_mode":"light_mode";const o=`<div class="default-text">
                             <h1>Plant Care Guide</h1>
                             <p>Nurture Your Plants to Perfection with GreenScape's Plant Care Guide!</p>
@@ -21,3 +17,4 @@ window.onunload = function () { null };
                         <p>${c}</p>
                     </div>
                 </div>`,o=u(t,"outgoing");(n=s.querySelector(".default-text"))==null||n.remove(),s.appendChild(o),s.scrollTo(0,s.scrollHeight),setTimeout(L,500)};y.addEventListener("click",()=>{confirm("Are you sure you want to delete all the chats?")&&(localStorage.removeItem("all-chats"),h())});l.addEventListener("click",()=>{document.body.classList.toggle("light-mode"),localStorage.setItem("themeColor",l.innerText),l.innerText=document.body.classList.contains("light-mode")?"dark_mode":"light_mode"});const m=r.scrollHeight;r.addEventListener("input",()=>{r.style.height=`${m}px`,r.style.height=`${r.scrollHeight}px`});r.addEventListener("keydown",t=>{t.key==="Enter"&&!t.shiftKey&&window.innerWidth>800&&(t.preventDefault(),p())});h();g.addEventListener("click",p);
+                
