0fa49a0891be8ea0060e94fe124c2eeb4f0fadd7
DevarajB97 <94118453+DevarajB97@users.noreply.github.com>
2023-09-06 20:09:27 +0530

    Update steps.js

--- assets/steps.js
+++ assets/steps.js
@@ -1,7 +1,87 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const d of i.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&a(d)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();const r=document.querySelector("#chat-input"),g=document.querySelector("#send-btn"),s=document.querySelector(".chat-container"),l=document.querySelector("#theme-btn"),y=document.querySelector("#delete-btn");let c=null;const f="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",h=()=>{const t=localStorage.getItem("themeColor");document.body.classList.toggle("light-mode",t==="light_mode"),l.innerText=document.body.classList.contains("light-mode")?"dark_mode":"light_mode";const o=`<div class="default-text">
-                            <h1>Plant Care Guide</h1>
-                            <p>Nurture Your Plants to Perfection with GreenScape's Plant Care Guide!</p>
-                        </div>`;s.innerHTML=localStorage.getItem("all-chats")||o,s.scrollTo(0,s.scrollHeight)},u=(t,o)=>{const n=document.createElement("div");return n.classList.add("chat",o),n.innerHTML=t,n},v=async t=>{const o="https://api.openai.com/v1/completions",n=document.createElement("p"),a={method:"POST",headers:{"Content-Type":"application/json",Authorization:`Bearer ${f}`},body:JSON.stringify({model:"text-davinci-003",prompt:`I want you to act as a Botanist and provide me steps to grow a particular plant. I will provide you with a plant name and your role is to generate stepwise info about how to grow a plant. My first request is:${c}`,max_tokens:3e3,temperature:0,top_p:1,frequency_penalty:.5,presence_penalty:0})};try{const e=await(await fetch(o,a)).json();n.textContent=e.choices[0].text.trim()}catch{n.classList.add("error"),n.textContent="Oops! Something went wrong while retrieving the response. Please try again."}t.querySelector(".typing-animation").remove(),t.querySelector(".chat-details").appendChild(n),localStorage.setItem("all-chats",s.innerHTML),s.scrollTo(0,s.scrollHeight)},L=()=>{const o=u(`<div class="chat-content">
+
+const chatInput = document.querySelector("#chat-input");
+const sendButton = document.querySelector("#send-btn");
+const chatContainer = document.querySelector(".chat-container");
+const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
+
+let userText = null;
+const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+
+const loadDataFromLocalstorage = () => {
+    // Load saved chats and theme from local storage and apply/add on the page
+    const themeColor = localStorage.getItem("themeColor");
+
+    document.body.classList.toggle("light-mode", themeColor === "light_mode");
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+
+    const defaultText = `<div class="default-text">
+                            <h1>PlantGPT</h1>
+                            <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
+                        </div>`
+
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+    chatContainer.scrollTo(0, chatContainer.scrollHeight); // Scroll to bottom of the chat container
+}
+
+const createChatElement = (content, className) => {
+    // Create new div and apply chat, specified class and set html content of div
+    const chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat", className);
+    chatDiv.innerHTML = content;
+    return chatDiv; // Return the created chat div
+}
+
+const getChatResponse = async (incomingChatDiv) => {
+    const API_URL = "https://api.openai.com/v1/completions";
+    const pElement = document.createElement("p");
+
+    // Define the properties and data for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: `I want you to act as a Botanist and provide me steps to grow a particular plant. I will provide you with a plant name and your role is to generate stepwise info about how to grow a plant. My first request is:${userText}`,
+            max_tokens: 3000,
+            temperature: 0,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+    }
+
+    // Send POST request to API, get response and set the reponse as paragraph element text
+    try {
+        const response = await (await fetch(API_URL, requestOptions)).json();
+        pElement.textContent = response.choices[0].text.trim();
+    } catch (error) { // Add error class to the paragraph element and set error text
+        pElement.classList.add("error");
+        pElement.textContent = "Oops! Something went wrong while retrieving the response. Please try again.";
+    }
+
+    // Remove the typing animation, append the paragraph element and save the chats to local storage
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+    localStorage.setItem("all-chats", chatContainer.innerHTML);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+}
+
+const copyResponse = (copyBtn) => {
+    // Copy the text content of the response to the clipboard
+    const reponseTextElement = copyBtn.parentElement.querySelector("p");
+    navigator.clipboard.writeText(reponseTextElement.textContent);
+    copyBtn.textContent = "done";
+    setTimeout(() => copyBtn.textContent = "content_copy", 1000);
+}
+
+const showTypingAnimation = () => {
+    // Display the typing animation and call the getChatResponse function
+    const html = `<div class="chat-content">
                     <div class="chat-details">
                         <img src="./images/chatbot.jpg" alt="chatbot-img">
                         <div class="typing-animation">
@@ -11,10 +91,68 @@
                         </div>
                     </div>
                     <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
-                </div>`,"incoming");s.appendChild(o),s.scrollTo(0,s.scrollHeight),v(o)},p=()=>{var n;if(c=r.value.trim(),!c)return;r.value="",r.style.height=`${m}px`;const t=`<div class="chat-content">
+                </div>`;
+    // Create an incoming chat div with typing animation and append it to chat container
+    const incomingChatDiv = createChatElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    getChatResponse(incomingChatDiv);
+}
+
+const handleOutgoingChat = () => {
+    userText = chatInput.value.trim(); // Get chatInput value and remove extra spaces
+    if(!userText) return; // If chatInput is empty return from here
+
+    // Clear the input field and reset its height
+    chatInput.value = "";
+    chatInput.style.height = `${initialInputHeight}px`;
+
+    const html = `<div class="chat-content">
                     <div class="chat-details">
                         <img src="images/user.jpg" alt="user-img">
-                        <p>${c}</p>
+                        <p>${userText}</p>
                     </div>
-                </div>`,o=u(t,"outgoing");(n=s.querySelector(".default-text"))==null||n.remove(),s.appendChild(o),s.scrollTo(0,s.scrollHeight),setTimeout(L,500)};y.addEventListener("click",()=>{confirm("Are you sure you want to delete all the chats?")&&(localStorage.removeItem("all-chats"),h())});l.addEventListener("click",()=>{document.body.classList.toggle("light-mode"),localStorage.setItem("themeColor",l.innerText),l.innerText=document.body.classList.contains("light-mode")?"dark_mode":"light_mode"});const m=r.scrollHeight;r.addEventListener("input",()=>{r.style.height=`${m}px`,r.style.height=`${r.scrollHeight}px`});r.addEventListener("keydown",t=>{t.key==="Enter"&&!t.shiftKey&&window.innerWidth>800&&(t.preventDefault(),p())});h();g.addEventListener("click",p);
-                
+                </div>`;
+
+    // Create an outgoing chat div with user's message and append it to chat container
+    const outgoingChatDiv = createChatElement(html, "outgoing");
+    chatContainer.querySelector(".default-text")?.remove();
+    chatContainer.appendChild(outgoingChatDiv);
+    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+    setTimeout(showTypingAnimation, 500);
+}
+
+deleteButton.addEventListener("click", () => {
+    // Remove the chats from local storage and call loadDataFromLocalstorage function
+    if(confirm("Are you sure you want to delete all the chats?")) {
+        localStorage.removeItem("all-chats");
+        loadDataFromLocalstorage();
+    }
+});
+
+themeButton.addEventListener("click", () => {
+    // Toggle body's class for the theme mode and save the updated theme to the local storage 
+    document.body.classList.toggle("light-mode");
+    localStorage.setItem("themeColor", themeButton.innerText);
+    themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
+});
+
+const initialInputHeight = chatInput.scrollHeight;
+
+chatInput.addEventListener("input", () => {   
+    // Adjust the height of the input field dynamically based on its content
+    chatInput.style.height =  `${initialInputHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If the Enter key is pressed without Shift and the window width is larger 
+    // than 800 pixels, handle the outgoing chat
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleOutgoingChat();
+    }
+});
+
+loadDataFromLocalstorage();
+sendButton.addEventListener("click", handleOutgoingChat);
