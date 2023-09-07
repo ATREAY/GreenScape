62b584fa95bde12d334ca9f37a741d65d4851bdd
DevarajB97 <94118453+DevarajB97@users.noreply.github.com>
2023-09-07 23:29:31 +0530

    Update steps.js

--- assets/steps.js
+++ assets/steps.js
@@ -1,19 +1,13 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const d of i.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&a(d)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();const r=document.querySelector("#chat-input"),g=document.querySelector("#send-btn"),s=document.querySelector(".chat-container"),l=document.querySelector("#theme-btn"),y=document.querySelector("#delete-btn");let c=null;const f="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",u=()=>{const t=localStorage.getItem("themeColor");document.body.classList.toggle("light-mode",t==="light_mode"),l.innerText=document.body.classList.contains("light-mode")?"dark_mode":"light_mode";const o=`<div class="default-text">
-                            <h1>Plant Care Guide</h1>
-                            <p>Nurture Your Plants to Perfection with GreenScape's Plant Care Guide!</p>
-                        </div>`;s.innerHTML=localStorage.getItem("all-chats")||o,s.scrollTo(0,s.scrollHeight)},h=(t,o)=>{const n=document.createElement("div");return n.classList.add("chat",o),n.innerHTML=t,n},v=async t=>{const o="https://api.openai.com/v1/completions",n=document.createElement("p"),a={method:"POST",headers:{"Content-Type":"application/json",Authorization:`Bearer ${f}`},body:JSON.stringify({model:"text-davinci-003",prompt:`I want you to act as a Botanist and provide me steps to grow a particular plant. I will provide you with a plant name and your role is to generate stepwise info about how to grow a plant. My first request is:${c}`,max_tokens:3e3,temperature:0,top_p:1,frequency_penalty:.5,presence_penalty:0})};try{const e=await(await fetch(o,a)).json();n.textContent=e.choices[0].text.trim()}catch{n.classList.add("error"),n.textContent="Oops! Something went wrong while retrieving the response. Please try again."}t.querySelector(".typing-animation").remove(),t.querySelector(".chat-details").appendChild(n),localStorage.setItem("all-chats",s.innerHTML),s.scrollTo(0,s.scrollHeight)},L=()=>{const o=h(`<div class="chat-content">
-                    <div class="chat-details">
-                        <img src="./images/chatbot.jpg" alt="chatbot-img">
-                        <div class="typing-animation">
-                            <div class="typing-dot" style="--delay: 0.2s"></div>
-                            <div class="typing-dot" style="--delay: 0.3s"></div>
-                            <div class="typing-dot" style="--delay: 0.4s"></div>
-                        </div>
-                    </div>
-                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
-                </div>`,"incoming");s.appendChild(o),s.scrollTo(0,s.scrollHeight),v(o)},m=()=>{var n;if(c=r.value.trim(),!c)return;r.value="",r.style.height=`${p}px`;const t=`<div class="chat-content">
-                    <div class="chat-details">
-                        <img src="images/user.jpg" alt="user-img">
-                        <p>${c}</p>
-                    </div>
-                </div>`,o=h(t,"outgoing");(n=s.querySelector(".default-text"))==null||n.remove(),s.appendChild(o),s.scrollTo(0,s.scrollHeight),setTimeout(L,500)};y.addEventListener("click",()=>{confirm("Are you sure you want to delete all the chats?")&&(localStorage.removeItem("all-chats"),u())});l.addEventListener("click",()=>{document.body.classList.toggle("light-mode"),localStorage.setItem("themeColor",l.innerText),l.innerText=document.body.classList.contains("light-mode")?"dark_mode":"light_mode"});const p=r.scrollHeight;r.addEventListener("input",()=>{r.style.height=`${p}px`,r.style.height=`${r.scrollHeight}px`});r.addEventListener("keydown",t=>{t.key==="Enter"&&!t.shiftKey&&window.innerWidth>800&&(t.preventDefault(),m())});u();g.addEventListener("click",m);
+(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const n of e)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const n={};return e.integrity&&(n.integrity=e.integrity),e.referrerpolicy&&(n.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?n.credentials="include":e.crossorigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function s(e){if(e.ep)return;e.ep=!0;const n=o(e);fetch(e.href,n)}})();const f="./images/assets/bot.svg",m="./images/assets/user.svg",c=document.querySelector("form"),a=document.querySelector("#chat_container");let d;function p(t){t.textContent="",d=setInterval(()=>{t.textContent+=".",t.textContent==="...."&&(t.textContent="")},300)}function g(t,r){let o=0,s=setInterval(()=>{o<r.length?(t.innerHTML+=r.charAt(o),o++):clearInterval(s)},20)}function h(){const t=Date.now(),o=Math.random().toString(16);return`id-${t}-${o}`}function l(t,r,o){return`
+        <div class="wrapper ${t&&"ai"}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${t?f:m} 
+                      alt="${t?"bot":"user"}" 
+                    />
+                </div>
+                <div class="message" id=${o}>${r}</div>
+            </div>
+        </div>
+    `}const u=async t=>{t.preventDefault();const r=new FormData(c);a.innerHTML+=l(!1,r.get("prompt")),c.reset();const o=h();a.innerHTML+=l(!0," ",o),a.scrollTop=a.scrollHeight;const s=document.getElementById(o);p(s);const e=await fetch("https://plantgpt3-5.onrender.com",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({prompt:r.get("prompt")})});if(clearInterval(d),s.innerHTML=" ",e.ok){const i=(await e.json()).bot.trim();g(s,i)}else{const n=await e.text();s.innerHTML="Something went wrong",alert(n)}};c.addEventListener("submit",u);c.addEventListener("keyup",t=>{t.keyCode===13&&u(t)});
